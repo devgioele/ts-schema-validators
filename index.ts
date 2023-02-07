09a3a43773bad1cd1117f628936e004b4c78a554
devgioele <gioele.devitti@aboutbits.it>
2023-02-07 12:46:51 +0100

    expand showcase

--- index.ts
+++ index.ts
@@ -1,12 +1,11 @@
 import yup from "yup";
+import { z } from "zod";
 
 const schema = yup.object({
-  a: yup.string().optional(),
-  b: yup
-    .object({
-      nested1: yup.number(),
-    })
-    .optional(),
+  a: yup.string(),
+  b: yup.object({
+    nested1: yup.number(),
+  }),
   c: yup.string().required(),
   d: yup.object().when("c", {
     is: "apple",
@@ -26,3 +25,40 @@ const test3: InferredType = {
   c: "apple",
   d: { count: 2 },
 };
+
+type A = {
+  count: number;
+};
+
+type B = {
+  date: string;
+};
+
+type C = ({ count: 2 } & B) | A;
+
+type D = {
+  id: string;
+} & ({ type: "A"; data: A } | { type: "B"; data: B });
+
+const x: D = {
+  id: "asdf",
+  type: "B",
+  data: {
+    date: "asfd",
+  },
+};
+
+const value1: C = { count: 2, date: "" };
+const value2: C = { count: 3, date: "" };
+
+const zodSchemaA = z.object({ count: z.number() });
+const zodSchemaB = z.object({ date: z.string() });
+const zodSchemaD = z
+  .object({ id: z.string() })
+  .and(
+    z
+      .object({ type: z.literal("A"), data: zodSchemaA })
+      .or(z.object({ type: z.literal("B"), data: zodSchemaB }))
+  );
+
+type InferredZodD = z.infer<typeof zodSchemaD>;
